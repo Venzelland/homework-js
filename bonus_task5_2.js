297852a936fe5d249b1c2bc1c052ab438571a308
PavielVolodko <pavielvolodko1997@mail.ru>
2025-02-20 18:22:01 +0300

    Добавлено решение домашнего задания 5.2 бонус (оптимизация)

--- bonus_task5_2.js
+++ bonus_task5_2.js
@@ -16,3 +16,30 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const total = 13;
 
 console.log(findPairWithSum(arr, total)); // [4, 9]
+
+// можем оптимизировать без дополнительной памяти, но если массив отсортированный(в принципе как у нас и есть)
+
+function findPair(arr, total) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        let sum = arr[left] + arr[right];
+
+        if (sum === total) {
+            return [arr[left], arr[right]];
+        } else if (sum < total) {
+            left++; // увеличиваем левый указатель
+        } else {
+            right--; // уменьшаем правый указатель
+        }
+    }
+
+    return null; // если пара не найдена
+}
+
+console.log(findPair(arr, total)); // [4, 9]
+
+
+
+
